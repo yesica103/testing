cf2bb62f0afb0f3411c7992db9ef46b09f7def39
Yesica Giraldo <yagiraldot@ieee.org>
2025-06-23 18:12:37 -0500

    Agregue logs para verificar cada test con eñ borrador

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,10 +1,19 @@
-import unittest
+import unittest 
 from src.bank_account import BankAccount
 
 class BankAccountTests(unittest.TestCase):
     def setUp(self) -> None:
+        import os
         self.account = BankAccount(balance = 1000, log_file = 'transaction_log.txt')
-        
+    
+    def tearDown(self) -> None:
+        import os
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+    
+    def _count_lines(self, filename):
+        with open(filename, 'r') as r:
+            return len(r.readlines())
 
     def test_deposit(self):
         new_balance = self.account.deposit(500)
@@ -31,7 +40,11 @@ class BankAccountTests(unittest.TestCase):
             self.account.transfer(1200, other_account)
 
     def test_transaction_log(self):
+        import os
         self.account.deposit(500)
-        
+        assert os.path.exists(self.account.log_file)
 
-        
+    def test_count_transactions(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(500)
+        assert self._count_lines(self.account.log_file) == 2
