07ef6a4cd9d6b1fbb5d05dc58a2c50ea7843774b
Yesica Giraldo <yagiraldot@ieee.org>
2025-06-23 11:49:12 -0500

    actualizaciones en la  calculadora

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,13 @@
 def sum (a,b):
     return a + b
 
-def substract (a,b):
+def substract (a, b):
     return a - b
+
+def multiply (a, b):
+    return a * b
+
+def divide (a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
--- tests/tests_calculator.py
+++ tests/tests_calculator.py
@@ -1,8 +1,16 @@
 import unittest
-from src.calculator import sum, substract
+from src.calculator import sum, substract, multiply, divide
 class CalculatorTests(unittest.TestCase):
     def test_sum(self):
         assert sum(2, 3) == 5
 
     def test_subtraction(self):
         assert substract(5, 2) == 3
+
+    def test_multiplication(self):
+        assert multiply(2, 3) == 6
+
+    def test_division(self):
+        assert divide(6, 3) == 2
+        with self.assertRaises(ValueError):
+            divide(5, 0)
