d70490c640f8208e8d85eb71748cb48f77539f22
Yesica Giraldo <yagiraldot@ieee.org>
2025-06-27 16:49:51 -0500

    Pruebas hoy con side-effect

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,9 +1,11 @@
-import unittest
+import unittest, requests
+import unittest.mock
 from src.api_client import get_location
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 
 
 class ApiClientTests(unittest.TestCase):
+
     @patch('src.api_client.requests.get')
     def test_get_location_returns_expected_data(self, mock_get):
         mock_get.return_value_status_code = 200
@@ -20,5 +22,32 @@ class ApiClientTests(unittest.TestCase):
 
 
         mock_get.assert_called_once_with('https://freeipapi.com/api/json/8.8.8.8')
+
+    @patch('src.api_client.requests.get')
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [
+            requests.exceptions.RequestException('Service Unavailable'),
+            unittest.mock.Mock(
+                status_code=200,
+                json=lambda: {
+                    'countryName': 'USA',
+                    'regionName': 'FLORIDA',
+                    'cityName': 'MIAMI'
+                }
+            )
+        ]
+
+           
+        
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location('8.8.8.8')
+       
+        result = get_location('8.8.8.8')
+        self.assertEqual(result.get('country'), 'USA')
+        self.assertEqual(result.get('region'), 'FLORIDA')
+        self.assertEqual(result.get('city'), 'SAN FRANCISCO')
+
+        
+
 
         
